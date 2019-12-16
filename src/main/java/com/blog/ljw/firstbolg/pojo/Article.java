a96ccdce922aa820bfc7531feae0e821f9cafc66
xiaowei <jwliu0213@163.com>
2019-12-16 10:56:01 +0800

    lombok插件修改

--- src/main/java/com/blog/ljw/firstbolg/pojo/Article.java
+++ src/main/java/com/blog/ljw/firstbolg/pojo/Article.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 import java.util.Date;
 
+@Data
 public class Article {
     private int id;
     private String title;
@@ -12,62 +13,6 @@ public class Article {
     private Date createdDate;
     private int commentCount;
     private String category;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescribes() {
-        return describes;
-    }
-
-    public void setDescribes(String describes) {
-        this.describes = describes;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public Date getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(Date createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public int getCommentCount() {
-        return commentCount;
-    }
-
-    public void setCommentCount(int commentCount) {
-        this.commentCount = commentCount;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
 
     @Override
     public String toString(){
